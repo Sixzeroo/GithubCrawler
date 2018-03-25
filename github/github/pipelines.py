f138750efac1bab041e5a4691abc71dfb38f38b6
sixzeroo <sixzeroo60@gmail.com>
2018-03-25 13:45:52 +0800

    [fix] 为user和rep添加剩下的索引，为了支持排序

--- github/github/pipelines.py
+++ github/github/pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-from pymongo import MongoClient,DESCENDING
+from pymongo import MongoClient,DESCENDING,IndexModel
 from pymongo.errors import DuplicateKeyError
 import redis
 from scrapy.exceptions import DropItem
@@ -26,8 +26,17 @@ class GithubPipeline(object):
         self.collection1 = db['user']
         # 建立唯一降序索引，实现数据库端去重
         self.collection1.create_index([("user_id",DESCENDING)], unique=True)
+        index1 = IndexModel([('stars_num',DESCENDING)])
+        index2 = IndexModel([('reps_num',DESCENDING)])
+        index3 = IndexModel([('following_num',DESCENDING)])
+        index4 = IndexModel([('follower_num',DESCENDING)])
+        self.collection1.create_indexes([index1, index2, index3, index4])
         self.collection2 = db['rep']
         self.collection2.create_index([("rep_id", DESCENDING)], unique=True)
+        index1 = IndexModel([('stars_num', DESCENDING)])
+        index2 = IndexModel([('commits_num', DESCENDING)])
+        index3 = IndexModel([('forks_num', DESCENDING)])
+        self.collection2.create_indexes([index1, index2, index3])
                 
 
     def process_item(self, item, spider):
