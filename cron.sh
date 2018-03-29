ccdb69c3c2ea607f5e0e32fb387c2646c7222752
Sixzeroo <sixzeroo60@gmail.com>
2018-03-29 14:58:09 +0800

    [fix] docker容器适应部署环境

--- cron.sh
+++ cron.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run -v ./github : /code --name github_cookie_update sixzeroo/github_crawler:0.1 "yacron -c crontab.yaml"
+sudo docker run -v pwd/github : /code --name github_cookie_update sixzeroo/github_crawler:0.1 "yacron -c crontab.yaml"
